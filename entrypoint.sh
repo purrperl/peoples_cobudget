b02aa18a28a5f15d1fa6d28882f85a039d1befcf
DJ Purrperl <purrperl.dude@gmail.com>
2026-03-28 11:26:19 +0900

    Add a message to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 
 echo "Running database migrations..."
-psql "$DATABASE_URL" -f /app/init.sql
-echo "Database ready."
+psql "$DATABASE_URL" -v ON_ERROR_STOP=1 -f /app/init.sql && echo "Database ready." || echo "Migration failed!"
 
 exec hypnotoad -f app.pl
